23ac996d94d0981ff9d40e19d19145e1b530ead6
Valeriy_Vasilkovskiy <valera_vasilkovskiy@mail.ru>
2019-09-15 16:54:26 +0300

    1

--- src/Katalog.java
+++ src/Katalog.java
@@ -1,9 +1,6 @@
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
 public class Katalog {
@@ -22,8 +19,33 @@ public class Katalog {
         }
         File[] files = new File(path).listFiles();
         assert files != null;
-        for (File f:files) {
-            Files.copy(f.toPath(),new File(dir.getAbsolutePath()+"/"+f.getName()).toPath(),StandardCopyOption.REPLACE_EXISTING);
+        for (File f : files) {
+            File newFile = new File(dir.getAbsolutePath(), f.getName());
+            Path file = f.toPath();
+
+            cop(file, newFile.getPath());
+
+//            Files.copy(f.toPath(),new File(dir.getAbsolutePath()+"/"+f.getName()).toPath(),StandardCopyOption.REPLACE_EXISTING);
         }
     }
+
+    private void cop(Path file, String fileCopy) throws IOException {
+        InputStream is = null;                                     //Поток чтения
+        OutputStream os = null;                                    //Поток записи
+        try {
+            is = new FileInputStream(String.valueOf(file));                 //Для чтения информации из файла
+            os = new FileOutputStream(String.valueOf(fileCopy));
+            byte[] buffer = new byte[1024];
+            int length;
+            while ((length = is.read(buffer)) > 0) {        //Возвращает количество прочитаных байтов
+                os.write(buffer, 0, length);              //записывает в поток байтов массива
+            }
+        } finally {
+            assert is != null;
+            is.close();
+            assert os != null;
+            os.close();             //закрывает поток
+        }
+    }
+
 }
